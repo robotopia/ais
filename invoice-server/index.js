f75479ca1a00aa0615b0cd71bf909cdee0eab1fa
Sam McSweeney <sammy.mcsweeney@gmail.com>
2023-08-05 15:06:11 +0800

    Added billing page. Not completely tested.

--- invoice-server/index.js
+++ invoice-server/index.js
@@ -120,3 +120,89 @@ app.post('/accounts', function(req, res) {
 
     res.redirect('/accounts');
 })
+
+
+function validateBillingForm(data) {
+    const str_re = /^[a-zA-Z0-9, ]+$/;
+    if (str_re.test(String(data.name)) == false) {
+        console.log("Invalid billing name (can contain only alphanumerics, commas, and spaces)");
+        return false;
+    }
+
+    if (str_re.test(String(data.addr1)) == false) {
+        console.log("Invalid address line 1 (can contain only alphanumerics and spaces)");
+        return false;
+    }
+
+    if (str_re.test(String(data.addr2)) == false) {
+        console.log("Invalid address line 2 (can contain only alphanumerics and spaces)");
+        return false;
+    }
+
+    const phone_re = /^[0-9 -+]+$/;
+    if (phone_re.test(String(data.phone)) == false) {
+        console.log("Invalid phone number");
+        return false;
+    }
+
+    const email_re = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    if (email_re.test(String(data.email)) == false) {
+        console.log("Invalid email address");
+        return false;
+    }
+
+    const abn_re = /^[0-9 -]+$/;
+    if (abn_re.test(String(data.abn)) == false) {
+        console.log("Invalid ABN");
+        return false;
+    }
+
+    if (str_re.test(String(data.bill_to)) == false) {
+        console.log("Invalid \"Bill to\" value");
+        return false;
+    }
+
+    return true;
+}
+
+app.get('/billing', function(req, res) {
+    if (!assert_connection(res)) {
+        return
+    }
+
+    con.query("SELECT * FROM billing", function(err, result, fields) {
+        res.render('billing', {
+            billing_fields: fields,
+            billings: result
+        });
+    });
+})
+
+app.post('/billing', function(req, res) {
+    if (!assert_connection(res)) {
+        return
+    }
+
+    if (validateBillingForm(req.body) == false) {
+        return;
+    }
+
+    if (req.body.action == "insert") {
+        con.query("INSERT INTO billing(name, addr1, addr2, phone, email, abn, bill_to) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            [req.body.name, req.body.addr1, req.body.addr2, req.body.phone, req.body.email, req.body.abn, req.body.bill_to]
+        );
+    }
+    else if (req.body.action == "delete") {
+        con.query("DELETE FROM billing WHERE id = ?",
+            [parseInt(req.body.id)]
+        );
+    }
+    else if (req.body.action == "update") {
+        con.query("UPDATE billing SET name = ?, addr1 = ?, addr2 = ?, phone = ?, email = ?, abn = ?, bill_to = ? WHERE id = ?",
+            [req.body.name, req.body.addr1, req.body.addr2, req.body.phone, req.body.email, req.body.abn, req.body.bill_to, parseInt(req.body.id)]
+        );
+    }
+
+    res.redirect('/billing');
+})
+
