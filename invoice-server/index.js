13acb2471d855e5a063cf348e3d658c7ceb5c74f
Sam McSweeney <sammy.mcsweeney@gmail.com>
2023-08-05 14:46:53 +0800

    Got Accounts page working. Now onto billing

--- invoice-server/index.js
+++ invoice-server/index.js
@@ -7,6 +7,7 @@ const mysql = require('mysql')
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 const db = "ppc_invoices";
@@ -57,17 +58,65 @@ app.post('/login', function(req, res) {
     });
 })
 
+function validateAccountForm(data) {
+    const bsb_re = /^[0-9]{3}-*[0-9]{3}$/;
+    if (bsb_re.test(String(data.bsb)) == false) {
+        console.log("Invalid BSB number");
+        return false;
+    }
+
+    const number_re = /^[0-9 -]+$/;
+    if (number_re.test(String(data.number)) == false) {
+        console.log("Invalid account number");
+        return false;
+    }
+
+    const name_re = /^[a-zA-Z0-9 ]+$/;
+    if (name_re.test(String(data.name)) == false) {
+        console.log("Invalid account name (can contain only alphanumerics and spaces)");
+        return false;
+    }
+
+    return true;
+}
+
 app.get('/accounts', function(req, res) {
     if (!assert_connection(res)) {
         return
     }
 
     con.query("SELECT * FROM account", function(err, result, fields) {
-        console.log(result);
-        console.log(fields);
         res.render('accounts', {
             account_fields: fields,
             accounts: result
         });
     });
+})
+
+app.post('/accounts', function(req, res) {
+    if (!assert_connection(res)) {
+        return
+    }
+
+    if (validateAccountForm(req.body) == false) {
+        return;
+    }
+
+    if (req.body.action == "insert") {
+        con.query("INSERT INTO account(bsb, number, name) VALUES (?, ?, ?)",
+            [req.body.bsb, req.body.number, req.body.name]
+        );
+    }
+    else if (req.body.action == "delete") {
+        con.query("DELETE FROM account WHERE id = ?",
+            [parseInt(req.body.id)]
+        );
+    }
+    else if (req.body.action == "update") {
+        con.query("UPDATE account SET bsb = ?, number = ?, name = ? WHERE id = ?",
+            [req.body.bsb, req.body.number, req.body.name, parseInt(req.body.id)]
+        );
+    }
+
+    res.redirect('/accounts');
 })
