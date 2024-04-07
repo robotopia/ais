e1e5e16400d143b182518bcb17c1868d94750a80
Sammy McSweeney <sammy.mcsweeney@gmail.com>
2024-04-07 08:24:10 +0800

    Started adding printable version of invoice as a view

--- django/invoice/urls.py
+++ django/invoice/urls.py
@@ -14,20 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
+from . import views
 
 urlpatterns = [
-    path('invoice/', include("invoice.urls")),
-    path("admin/", admin.site.urls),
+    re_path(r'^printable/(?P<pk>[0-9]+)$', views.printable_invoice, name='printable_invoice'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-admin.site.site_header = 'Automatic Invoicing System'
-admin.site.index_title = 'Home'
-admin.site.site_title = 'AIS'
-admin.site.site_url = None
--- django/invoice/views.py
+++ django/invoice/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from . import models
 
 # Create your views here.
+
+def printable_invoice(request, pk):
+
+    invoice = get_object_or_404(models.Invoice, pk=pk)
+
+    context = {
+        'invoice': invoice,
+    }
+
+    return render(request, 'invoice/printable_invoice.html', context)
